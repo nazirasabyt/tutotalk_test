d6af7d5225ada95e1a0953fd49584f4b8fae0f38
Nazira <nazirasabyt@gmail.com>
2024-01-11 15:09:15 +0400

    url

--- components/Lessons/RoomIdGenerator.js
+++ components/Lessons/RoomIdGenerator.js
@@ -1,34 +1,41 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 const RoomIdGenerator = () => {
-	const { asPath } = useRouter();
-	const router = useRouter();
-	const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+  const { asPath } = useRouter();
+  const router = useRouter();
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
 
-	const URL = `${origin}${asPath}`;
-	console.log(URL);
+  const URL = `${origin}${asPath}`;
+  console.log(URL);
 
-	const getId = async () => {
-		const response = await fetch(`http://localhost:3005/get-id`);
-		const data = await response.json();
-		return data;
-	};
+  const getId = async () => {
+    const response = await fetch(
+      `https://server-test-xb6v.onrender.com/get-id`
+    );
+    const data = await response.json();
+    return data;
+  };
 
-	const handleClick = async () => {
-		const id = await getId();
-		console.log(id);
-		if (id) {
-			router.push(`${URL}/room/${id.id}`);
-		}
-	};
+  const handleClick = async () => {
+    const id = await getId();
+    console.log(id);
+    if (id) {
+      router.push(`${URL}/room/${id.id}`);
+    }
+  };
 
-	return (
-		<div className=" hidden md:flex flex-col items-center justify-center fixed  top-0 right-0 w-[35%] gap-4 h-screen">
-			<button className="bg-blueSecondary text-white px-4 py-2 rounded-md hover:bg-blue-700 text-base font-medium" onClick={handleClick}>
-				Start Session
-			</button>
-		</div>
-	);
+  return (
+    <div className=' hidden md:flex flex-col items-center justify-center fixed  top-0 right-0 w-[35%] gap-4 h-screen'>
+      <button
+        className='bg-blueSecondary text-white px-4 py-2 rounded-md hover:bg-blue-700 text-base font-medium'
+        onClick={handleClick}>
+        Start Session
+      </button>
+    </div>
+  );
 };
 
 export default RoomIdGenerator;
--- components/Lessons/StudentVideoSession.js
+++ components/Lessons/StudentVideoSession.js
@@ -1,26 +1,28 @@
-import { useSocketContext } from '../../context/socket-context';
-import VideoCall from './VideoCall';
-import { useEffect } from 'react';
+import { useSocketContext } from "../../context/socket-context";
+import VideoCall from "./VideoCall";
+import { useEffect } from "react";
 
 const StudentVideoSession = ({ roomId, isTeacher, userName }) => {
-	const { setMyId, setRoomId, setName } = useSocketContext();
+  const { setMyId, setRoomId, setName } = useSocketContext();
 
-	useEffect(() => {
-		const getId = async () => {
-			const response = await fetch(`http://localhost:3005/get-id`);
-			const data = await response.json();
-			await setMyId(data.id);
-		};
+  useEffect(() => {
+    const getId = async () => {
+      const response = await fetch(
+        `https://server-test-xb6v.onrender.com/get-id`
+      );
+      const data = await response.json();
+      await setMyId(data.id);
+    };
 
-		getId();
-		setName(userName);
-		setRoomId(roomId);
-	}, []);
-	return (
-		<div className=" h-full flex flex-col">
-			<VideoCall roomId={roomId} />
-		</div>
-	);
+    getId();
+    setName(userName);
+    setRoomId(roomId);
+  }, []);
+  return (
+    <div className=' h-full flex flex-col'>
+      <VideoCall roomId={roomId} />
+    </div>
+  );
 };
 
 export default StudentVideoSession;
